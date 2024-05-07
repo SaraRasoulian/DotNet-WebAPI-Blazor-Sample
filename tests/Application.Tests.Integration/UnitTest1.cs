d4a7327324910910b13c000aba3a05c1f9582c1a
sara rasoulian <sara.rasoulian.dev@gmail.com>
2024-05-07 22:28:02 +0330

    Create clean architecture projects and test projects

---
+++ tests/Application.Tests.Integration/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Application.Tests.Integration;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/WebAPI.Tests.Integration/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace WebAPI.Tests.Integration
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
