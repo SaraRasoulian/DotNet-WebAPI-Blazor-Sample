8a30e8003d1b7aecc290f71ec053158a75afc13f
sara rasoulian <sara.rasoulian.dev@gmail.com>
2024-05-08 13:13:17 +0330

    Create Email value object

---
+++ src/Domain/ValueObjects/Email.cs
@@ -0,0 +1,34 @@
+﻿namespace Domain.ValueObjects;
+
+public record Email
+{
+    public string Value { get; private set; }
+
+    public Email(string value)
+    {
+        if (!IsEmailValid(value))
+            throw new InvalidDataException("Email is invalid");
+
+        Value = value.Trim().ToLower();
+    }
+
+    bool IsEmailValid(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+            return false;
+        if (!value.Contains('@'))
+            return false;
+        if (value.EndsWith("."))
+            return false;
+
+        try
+        {
+            var addr = new System.Net.Mail.MailAddress(value);
+            return addr.Address == value.Trim();
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
