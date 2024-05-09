60c7e55f90eeb35f77610ace915b8b0604c160b3
sara rasoulian <sara.rasoulian.dev@gmail.com>
2024-05-09 14:06:32 +0330

    Implement 'get all' api

---
+++ src/WebAPI/Controllers/StudentsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Application.Students.Queries;
+using Application.ResponseModels;
+using Mapster;
+using MediatR;
+
+namespace WebAPI.Controllers;
+
+[Route("api/students")]
+[ApiController]
+public class StudentsController : ControllerBase
+{
+    private readonly IMediator _mediator;
+    public StudentsController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        var query = new GetAllStudentsQuery();
+        var response = await _mediator.Send(query);
+        return Ok(response);
+    }
+}
