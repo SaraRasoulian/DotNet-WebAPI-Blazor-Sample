607247e656268c0b280671dd40e27e68236e2a8d
sara rasoulian <sara.rasoulian.dev@gmail.com>
2024-05-09 14:09:43 +0330

    Add integration test for 'get all students' handler

---
+++ tests/Application.Tests.Integration/Students/StudentHandlersTests.cs
@@ -0,0 +1,21 @@
+﻿using Application.Students.Queries;
+
+namespace Application.Tests.Integration.Students;
+
+public class StudentHandlersTests : BaseIntegrationTest
+{
+    public StudentHandlersTests(IntegrationTestWebApplicationFactory factory) : base(factory) { }
+
+    [Fact]
+    public async void GetAllCustomerHandler_Returns_CustomerResponse()
+    {
+        // Arrange
+        var query = new GetAllStudentsQuery();
+
+        // Act
+        var result = await _sender.Send(query);
+
+        // Assert
+        Assert.NotNull(result);
+    }
+}
