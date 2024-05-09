8b7eef3ec6d65e7cffdff50c0f31d33344352c98
sara rasoulian <sara.rasoulian.dev@gmail.com>
2024-05-09 14:09:27 +0330

    Set up WebApplicationFactory for integration tests

---
+++ tests/Application.Tests.Integration/BaseIntegrationTest.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application.Tests.Integration;
+
+public class BaseIntegrationTest : IClassFixture<IntegrationTestWebApplicationFactory>
+{
+    private readonly IServiceScope _serviceScope;
+    protected readonly ISender _sender;
+    protected BaseIntegrationTest(IntegrationTestWebApplicationFactory factory)
+    {
+        _serviceScope = factory.Services.CreateScope();
+        _sender = _serviceScope.ServiceProvider.GetRequiredService<ISender>();
+    }
+}
