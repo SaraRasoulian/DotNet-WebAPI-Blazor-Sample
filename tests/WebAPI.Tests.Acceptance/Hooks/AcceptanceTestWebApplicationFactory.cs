7bfd41d25508881eec85122c1b24240ef2a93305
sara rasoulian <sara.rasoulian.dev@gmail.com>
2024-05-10 22:06:39 +0330

    Set up web application factory for acceptance testing

---
+++ tests/WebAPI.Tests.Acceptance/Hooks/AcceptanceTestWebApplicationFactory.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Hosting;
+using Infrastructure.DbContexts;
+using Testcontainers.PostgreSql;
+using TechTalk.SpecFlow;
+
+namespace WebAPI.Tests.Acceptance.Hooks;
+
+[Binding]
+public class AcceptanceTestWebApplicationFactory : WebApplicationFactory<Program>, IAsyncLifetime
+{
+    private readonly PostgreSqlContainer _dbContainer = new PostgreSqlBuilder()
+        .WithImage("postgres:latest")
+        .WithDatabase("StudentManagementDB_Test")
+        .WithUsername("postgres")
+        .WithPassword("mysecretpassword")
+        .Build();
+
+    [BeforeScenario]
+    public async Task InitializeAsync()
+    {
+        await _dbContainer.StartAsync();
+
+        using (var scope = Services.CreateScope())
+        {
+            var scopedServices = scope.ServiceProvider;
+            var cntx = scopedServices.GetRequiredService<StudentManagementDBContext>();
+
+            await cntx.Database.EnsureCreatedAsync();
+        }
+    }
+
+    [AfterScenario]
+    public new async Task DisposeAsync()
+    {
+        await _dbContainer.StopAsync();
+    }
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureTestServices(services =>
+        {
+            var descriptor = services.SingleOrDefault(s => s.ServiceType == typeof(DbContextOptions<StudentManagementDBContext>));
+
+            if (descriptor is not null)
+            {
+                services.Remove(descriptor);
+            }
+
+            services.AddDbContext<StudentManagementDBContext>(options =>
+            {
+                options.UseNpgsql(_dbContainer.GetConnectionString());
+            });
+        });
+    }
+}
