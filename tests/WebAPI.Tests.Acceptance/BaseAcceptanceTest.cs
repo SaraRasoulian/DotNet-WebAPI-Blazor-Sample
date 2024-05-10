b08d5afca41bf5102d34f541d2110fe95a6aae86
sara rasoulian <sara.rasoulian.dev@gmail.com>
2024-05-10 22:08:58 +0330

    Implement student step definition

---
+++ tests/WebAPI.Tests.Acceptance/BaseAcceptanceTest.cs
@@ -0,0 +1,13 @@
+﻿using WebAPI.Tests.Acceptance.Hooks;
+
+namespace WebAPI.Tests.Acceptance;
+
+public abstract class BaseAcceptanceTest : IClassFixture<AcceptanceTestWebApplicationFactory>
+{
+    protected readonly HttpClient _httpClient;
+
+    protected BaseAcceptanceTest(AcceptanceTestWebApplicationFactory factory)
+    {
+        _httpClient = factory.CreateDefaultClient();
+    }
+}
