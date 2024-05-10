7442e188537d257690210b873e872fa333d4d99b
sara rasoulian <sara.rasoulian.dev@gmail.com>
2024-05-10 18:57:17 +0330

    Add integration test for student post api

--- tests/WebAPI.Tests.Integration/ControllersTests/StudentsControllerTests.cs
+++ tests/WebAPI.Tests.Integration/ControllersTests/StudentsControllerTests.cs
@@ -1,4 +1,8 @@
 ﻿using Application.Tests.Integration;
+using Application.ResponseModels;
+using Application.RequestModels;
+using System.Net.Http.Json;
+using Domain.ValueObjects;
 
 namespace WebAPI.Tests.Integration.ControllersTests;
 
@@ -18,5 +22,30 @@ public class StudentsControllerTests : BaseControllerTest
 
         // Assert
         response.EnsureSuccessStatusCode();
+    }
+
+    [Fact]
+    public async Task Post_Returns_OK_With_StudentResponse()
+    {
+        // Arrange
+        CreateStudentRequest request = new CreateStudentRequest(
+            FirstName: "Max",
+            LastName: "Mirzaee",
+            Email: new Email("max@gmail.com"),
+            BirthDate: new DateOnly(2006, 06, 01),
+            GitHubUsername: "max-1");
+
+        // Act
+        var response = await _httpClient.PostAsJsonAsync("/api/students", request);
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+
+        var studentResponse = await response.Content.ReadFromJsonAsync<StudentResponse>();
+        Assert.Equal(request.FirstName, studentResponse.FirstName);
+        Assert.Equal(request.LastName, studentResponse.LastName);
+        Assert.Equal(request.Email.Value, studentResponse.Email.Value);
+        Assert.Equal(request.BirthDate, studentResponse.BirthDate);
+        Assert.Equal(request.GitHubUsername, studentResponse.GitHubUsername);
     }
 }
