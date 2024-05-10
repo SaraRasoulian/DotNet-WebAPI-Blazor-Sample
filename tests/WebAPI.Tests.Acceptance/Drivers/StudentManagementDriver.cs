bde88e77a37ad90cfcabfe49b8fb4cc67272f3e2
sara rasoulian <sara.rasoulian.dev@gmail.com>
2024-05-10 22:08:21 +0330

    Implement driver design pattern

---
+++ tests/WebAPI.Tests.Acceptance/Drivers/StudentManagementDriver.cs
@@ -0,0 +1,49 @@
+﻿using WebAPI.Tests.Acceptance.Hooks;
+using WebAPI.Tests.Acceptance.Dtos;
+using TechTalk.SpecFlow.Assist;
+using System.Globalization;
+using System.Net.Http.Json;
+using Domain.ValueObjects;
+using TechTalk.SpecFlow;
+
+namespace WebAPI.Tests.Acceptance.Drivers;
+
+public class StudentManagementDriver : BaseAcceptanceTest
+{
+    public StudentManagementDriver(AcceptanceTestWebApplicationFactory factory) : base(factory)
+    {
+    }
+
+    public List<StudentDto> GetAllStudents()
+    {
+        var response = _httpClient.GetAsync("/api/students").Result;
+        return response.Content.ReadFromJsonAsync<List<StudentDto>>().Result;
+    }
+
+    public HttpResponseMessage CreateStudent(Table student)
+    {
+        var request = ParseCreateStudentData(student);
+        return _httpClient.PostAsJsonAsync("/api/students", request).Result;
+    }
+
+    public HttpResponseMessage UpdateStudent(long studentId, Table student)
+    {
+        var request = ParseCreateStudentData(student);
+        request.Id = studentId;
+
+        return _httpClient.PutAsJsonAsync("/api/students/" + studentId, request).Result;
+    }
+
+    public HttpResponseMessage DeleteStudent(long studentId)
+    {
+        return _httpClient.DeleteAsync("/api/students/" + studentId).Result;
+    }
+
+    public StudentDto ParseCreateStudentData(Table student)
+    {
+        var request = student.CreateInstance<StudentDto>();
+        request.BirthDate = DateOnly.ParseExact(student.Rows[0]["BirthDate"], "yyyy-MM-dd", CultureInfo.InvariantCulture);
+        request.Email = new Email(student.Rows[0]["Email"]);
+        return request;
+    }
+}
---
+++ tests/WebAPI.Tests.Acceptance/Dtos/StudentDto.cs
@@ -0,0 +1,18 @@
+﻿using Domain.ValueObjects;
+
+namespace WebAPI.Tests.Acceptance.Dtos;
+
+public record StudentDto
+{
+    public long Id { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public Email Email { get; set; } = null!;
+
+    public DateOnly BirthDate { get; set; }
+
+    public string GitHubUsername { get; set; } = null!;
+}
