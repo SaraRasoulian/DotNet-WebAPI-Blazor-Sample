bcd5ee1f148e1338c0e097bd88caea0c11823217
sara rasoulian <sara.rasoulian.dev@gmail.com>
2024-05-11 18:04:24 +0330

    Update base URL to reference API app running inside the Docker container
    This reverts commit cf735f11c17c46aeb74da28f7b07ef0ed9929af8.

--- server/src/WebAPI/Program.cs
+++ server/src/WebAPI/Program.cs
@@ -14,7 +14,11 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-app.UseCors(options => options.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+// Enable CORS for the client app running inside a Docker container
+app.UseCors(options => options.WithOrigins("http://localhost:8080").AllowAnyHeader().AllowAnyMethod());
+
+// Enable CORS for the client app running on localhost
+app.UseCors(options => options.WithOrigins("https://localhost:7147").AllowAnyHeader().AllowAnyMethod());
 
 if (app.Environment.IsDevelopment())
 {
