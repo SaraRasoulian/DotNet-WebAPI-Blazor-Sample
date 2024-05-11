575658ce04a345b511994e52645521ebdd387c33
sara rasoulian <sara.rasoulian.dev@gmail.com>
2024-05-11 16:12:10 +0330

    Implement student dto in client app

---
+++ client/src/WebUI/Dtos/StudentDto.cs
@@ -0,0 +1,21 @@
+﻿namespace WebUI.Dtos;
+
+public record StudentDto
+{
+    public long Id { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public Email Email { get; set; } = null!;
+
+    public DateOnly BirthDate { get; set; }
+
+    public string GitHubUsername { get; set; } = null!;
+}
+
+public record Email
+{
+    public string Value { get; set; } = null!;
+}
