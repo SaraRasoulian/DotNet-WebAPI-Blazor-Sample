1398025c91ac64c46efd6d46c5dc37a966fdced1
sara rasoulian <sara.rasoulian.dev@gmail.com>
2024-05-11 16:13:49 +0330

    Implement student service to make http requests

---
+++ client/src/WebUI/Services/ApiUrls.cs
@@ -0,0 +1,6 @@
+﻿namespace WebUI.Services;
+
+public static class ApiUrls
+{
+    public const string BaseURL = "https://localhost:7120";
+}
---
+++ client/src/WebUI/Services/StudentService.cs
@@ -0,0 +1,40 @@
+﻿using System.Net.Http.Json;
+using WebUI.Dtos;
+
+namespace WebUI.Services;
+
+public class StudentService
+{
+    private const string URL = ApiUrls.BaseURL + "/api/students";
+    private readonly HttpClient _httpClient;
+
+    public StudentService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<StudentDto[]> GetAll()
+    {
+        return await _httpClient.GetFromJsonAsync<StudentDto[]>(URL);
+    }
+
+    public async Task<StudentDto> Get(string studentId)
+    {
+        return await _httpClient.GetFromJsonAsync<StudentDto>($"{URL}/{studentId}");
+    }
+
+    public async Task<HttpResponseMessage> Add(StudentDto student)
+    {
+        return await _httpClient.PostAsJsonAsync(URL, student);
+    }
+
+    public async Task<HttpResponseMessage> Edit(string studentId, StudentDto StudentDto)
+    {
+        return await _httpClient.PutAsJsonAsync($"{URL}/{studentId}", StudentDto);
+    }
+
+    public async Task<HttpResponseMessage> Delete(string studentId)
+    {
+        return await _httpClient.DeleteAsync($"{URL}/{studentId}");
+    }
+}
