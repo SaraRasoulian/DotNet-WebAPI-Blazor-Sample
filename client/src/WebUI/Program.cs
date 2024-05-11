e9859b9cf483aeb01c054f90c5931735f6f94c6e
sara rasoulian <sara.rasoulian.dev@gmail.com>
2024-05-11 16:15:29 +0330

    Implement layout Add images Implement style

--- client/src/WebUI/Program.cs
+++ client/src/WebUI/Program.cs
@@ -1,5 +1,6 @@
-using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.Components.Web;
+using WebUI.Services;
 using WebUI;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -7,5 +8,8 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddScoped<StudentService>();
+builder.Services.AddBlazorBootstrap();
 
 await builder.Build().RunAsync();
